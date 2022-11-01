756cc8c965c4336e1878c56a2c62b36f0a7b90df
Evangel90 <oladipoevangel90@gmail.com>
2022-11-01 12:49:56 +0100

    new changes

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,9 +1,10 @@
 import '../styles/profile.css';
-import profile_img from '../img/profile__img.png'
+import profile_img from '../img/profile__img.png';
 
 const Profile = () => {
     return(
         <div id='profile'>
+            {/* <div className='imgWrapper'></div> */}
             <img id='profile_img' src={profile_img} alt="profile_img"/>
             <h1 id='twitter'>Annette Black</h1>
         </div>
